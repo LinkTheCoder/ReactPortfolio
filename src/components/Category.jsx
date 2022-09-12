410c3666591417b78ae44aae3b363b8e690b5382
Link0w0 <66062268+LinkTheCoder@users.noreply.github.com>
2022-09-12 12:15:15 +0200

    Update

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,22 +5,13 @@ const Category = () => {
   console.log(categories);
   return (
     <div className='max-w-[1640px] m-auto px-4 py-12'>
-      <h1 className='text-orange-600 font-bold text-4xl text-center'>
-        Top Rated Menu Items
+      <h1 className='text-yellow-500 font-bold text-4xl text-center'>
+        Hitta Oss
       </h1>
-      {/* Categories */}
-      <div className='grid grid-cols-2 md:grid-cols-4 gap-6 py-6'>
-        {categories.map((item, index) => (
-          <div
-            key={index}
-            className='bg-gray-100 rounded-lg p-4 flex justify-between items-center'
-          >
-            <h2 className='font-bold sm:text-xl'>{item.name}</h2>
-            <img src={item.image} alt={item.name} className='w-20' />
-          </div>
-        ))}
+<div align="center">
+      <iframe className='rounded-xl mt-4 w-full aspect-auto' title="Google Maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1760.1452508885463!2d20.281693216216002!3d63.822130850013544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x467c5b0596b7c201%3A0x50415757cd11359a!2sRestaurang%20%26%20Pizzeria%20Matador!5e0!3m2!1ssv!2sse!4v1662550826900!5m2!1ssv!2sse" height="400em" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       </div>
-    </div>
+      </div>
   );
 };
 
--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -2,49 +2,59 @@ import React from 'react';
 
 const HeadlineCards = () => {
   return (
-    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+    <div>
+    <h1 className=' mt-6 mb-2 text-yellow-500 font-bold text-4xl text-center'>
+    Lunch Menyer
+  </h1>
+  
+    <div className='max-w-[1640px] mx-auto p-4 grid md:grid-cols-3 gap-6'>
+
       {/* Card */}
       <div className='rounded-xl relative'>
         {/* Overlay */}
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-          <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
-          <p className='px-2'>Through 8/26</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <p className='font-bold text-2xl px-2 pt-4'>Kebab/Pizzatallrik</p>
+          <p className='px-2'>Tills 10/12 (10:30 - 15:00)</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>90:-*</button>
         </div>
         <img
         className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
-          src='https://images.unsplash.com/photo-1613769049987-b31b641f25b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGJyZWFrZmFzdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'
+          src='/images/KebabLunch.jpg'
           alt='/'
         />
       </div>
+
       {/* Card */}
       <div className='rounded-xl relative'>
         {/* Overlay */}
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-          <p className='font-bold text-2xl px-2 pt-4'>New Restaurants</p>
-          <p className='px-2'>Added Daily</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <p className='font-bold text-2xl px-2 pt-4'>Salladstallrik</p>
+          <p className='px-2'>Tills 10/12 (10:30 - 15:00)</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>90:-*</button>
         </div>
         <img
         className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
-          src='https://images.unsplash.com/photo-1544025162-d76694265947?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJicXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'
+          src='/images/SalladTallrik.jpg'
           alt='/'
         />
       </div>
+
       {/* Card */}
       <div className='rounded-xl relative'>
         {/* Overlay */}
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-          <p className='font-bold text-2xl px-2 pt-4'>We Deliver Desserts Too</p>
-          <p className='px-2'>Tasty Treats</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <p className='font-bold text-2xl px-2 pt-4'>Husmanskosttallrik</p>
+          <p className='px-2'>Tills 10/12 (10:30 - 15:00)</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>90:-*</button>
         </div>
         <img
         className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
-          src='https://images.unsplash.com/photo-1559715745-e1b33a271c8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGRlc3NlcnR8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+          src='/images/Husmakost.jpg'
           alt='/'
         />
       </div>
+    </div>
+    <p className='text-center'>* Student och pensionärsrabatt 10%</p>
     </div>
   );
 };
